dc0a5522e9723100749eb06e3f417cc777d4ae56
ricie23 <ricierice23@gmail.com>
2025-07-31 21:37:36 -0400

    completed Functions module

--- Functions/Recursion/recursion.py
+++ Functions/Recursion/recursion.py
@@ -2,8 +2,9 @@ def factorial(n):
     if n <= 1:
         return 1
     else:
+        n *= factorial(n-1)
         # calculate n multiplied by the factorial of n - 1
-        return ???
+        return n
 
 
 print(factorial(12))
