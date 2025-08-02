c9d232e9f5511b18d51c784909a6ae9aa03226f2
ricie23 <ricierice23@gmail.com>
2025-08-02 14:55:58 -0400

    finished Files  module

--- File input output/Read all lines/read_all_lines.py
+++ File input output/Read all lines/read_all_lines.py
@@ -1,5 +1,6 @@
+lines_list = []
 with open("input.txt", "r") as infile:
-    # TODO: read all lines from input.txt into the list called lines_list
-
+    for line in infile:
+        lines_list.append(line)
 if __name__ == "__main__":
     print(lines_list)
