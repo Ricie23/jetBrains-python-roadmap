0bf12cf70ffa5d7a6b7445099da210a59785fcde
ricie23 <ricierice23@gmail.com>
2025-08-01 22:15:03 -0400

    finished Modules and Packages  module

--- Modules and packages/Executing modules as scripts/some_module.py
+++ Modules and packages/Executing modules as scripts/some_module.py
@@ -1,9 +1,6 @@
 def func():
     print('This is a message from the function in the imported module.')
 
-
 print(f'This is a message from {__name__}.')
-
-# Make a change here (add a main block)
-print('This should not be printed when this file is imported')
-
+if __name__ == '__main__':
+    func()
